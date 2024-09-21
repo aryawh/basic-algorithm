b7d6f21ee2d54d117b9ba9f3bac43ca5a3d01c45
Arya Widya Hastungkoro <88360900+aryawh@users.noreply.github.com>
2024-09-21 11:36:16 +0700

    add looping operation(faktorial, up and down, nest)

--- cPerulangan.java
+++ cPerulangan.java
@@ -47,13 +47,77 @@ class perpangkatan {
     }
 }
 
+// 1. LOOPING UP AND DOWN
+class loopingUpAndDown {
+    static void result() {
+        System.out.println("PERULANGAN MENAIK");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("PERULANGAN MENURUN");
+        for (int i = 5; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
+
+// 2. LOOPING BERSARANG
+class loopingBersarang {
+    void result() {
+        int j = 10;
+        int k;
+        int result;
+        while (j >= 1) {
+            k = 1;
+            while (k <= j) {
+                result = k * j;
+                System.out.print(result + " ");
+                k++;
+            }
+            System.out.println();
+            j--;
+        }
+    }
+}
+
+// 3. FAKTORIAL
+class realFaktorial {
+    static void result() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukan bilangan yang akan dihitung : ");
+        int value = scanner.nextInt();
+        int temp = 1;
+
+        System.out.print(value + "! = ");
+        while (value >= 1) {
+            temp *= value;
+            if (value != 1) {
+                System.out.print(value + " x ");
+            } else {
+                System.out.print(value + " = ");
+
+            }
+            value--;
+        }
+        System.out.println(temp);
+        scanner.close();
+    }
+}
+
 public class cPerulangan {
 
     fibbonaci f = new fibbonaci();
     static perpangkatan perpangkatan = new perpangkatan();
+    loopingUpAndDown loop = new loopingUpAndDown();
+    static loopingBersarang loopingBersarang = new loopingBersarang();
+    realFaktorial real = new realFaktorial();
 
     public static void main(String[] args) {
         // fibbonaci.result();
-        perpangkatan.result();
+        // perpangkatan.result();
+        // loopingUpAndDown.result();
+        // loopingBersarang.result();
+        realFaktorial.result();
     }
 }
